a7202242a2d9b8b6f51637ab45db0197cee55657
Younes Ghorbani <122295811+younesghorbani@users.noreply.github.com>
2023-03-06 23:27:22 +0330

    Set the desired number of squares dynamically

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+function setNumberOfSquares(event) {
+    rows[0].value = event.target.value;
+    columns[0].value = event.target.value;
+}
+
 const squares = document.getElementById('squares');
 const rows = document.getElementsByClassName('rows');
 const columns = document.getElementsByClassName('columns');
-
+    
 rows[0].value = squares.value;
 columns[0].value = squares.value;
+
+squares.addEventListener('input', setNumberOfSquares);
