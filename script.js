3f45b529f7bfd790cac8da9f26a251789bc167ae
Younes Ghorbani <122295811+younesghorbani@users.noreply.github.com>
2023-03-08 00:29:27 +0330

    Change how the # of squares is determined and placed
    - When the page loads, the default number of squares for each side
      is determined and placed.
    - When the user determines the number of squares on each side,
      their number is displayed momentarily and then placed on the page.

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-function setNumberOfSquares(event) {
-    rows[0].value = event.target.value;
-    columns[0].value = event.target.value;
+function setNumberOfSquares() {
+    rows[0].value = squares.value;
+    columns[0].value = squares.value;
+}
+
+function placeSquares() {
+    const grid = document.getElementsByClassName('grid');
+    if (grid[0].hasChildNodes()) grid[0].innerHTML = '';
+    grid[0].style.display = 'grid';
+    grid[0].style.gridTemplateColumns = `repeat(${squares.value}, 1fr)`;
+    grid[0].style.gridTemplateRows = `repeat(${squares.value}, 1fr)`;
+
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    const total = Math.pow(squares.value, 2);
+    for (let counter = 0; counter < total; counter++) {
+        grid[0].insertAdjacentHTML('beforeend', square.outerHTML);
+    }
 }
 
 const squares = document.getElementById('squares');
 const rows = document.getElementsByClassName('rows');
 const columns = document.getElementsByClassName('columns');
-    
-rows[0].value = squares.value;
-columns[0].value = squares.value;
 
+window.addEventListener('load', setNumberOfSquares);
+window.addEventListener('load', placeSquares);
 squares.addEventListener('input', setNumberOfSquares);
+squares.addEventListener('change', placeSquares);
