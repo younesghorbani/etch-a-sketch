cc1ced91c0d5a404dd644a1d3b8c1a894d929598
Younes Ghorbani <122295811+younesghorbani@users.noreply.github.com>
2023-03-12 22:49:39 +0330

    Add 'Normal' mode
    In this case, the page is filled by the color selected by the user.
    
    Also, a mechanism to activate different modes selected by the user
    was also added.

--- script.js
+++ script.js
@@ -17,21 +17,34 @@ function placeSquares() {
     }
 }
 
-function changeBackgroundColor(event) {
-    const color = document.getElementById('color');
+function useDefaultValues() {
+    setNumberOfSquares();
+    placeSquares();
+}
 
-    if (event.target.className === 'square') {
-        event.target.style.backgroundColor = color.value;
+function useNormalMode(event) {
+    if (event.target.className !== 'square normal') {
+        event.target.classList.add('normal');
+        event.target.style.opacity = '';
+    }
+
+    event.target.style.backgroundColor = color.value;
+}
+
+function useActivatedMode(event) {
+    if (event.target.id === 'normal') {
+        grid[0].addEventListener('mouseover', useNormalMode);
     }
 }
 
 const squares = document.getElementById('squares');
 const rows = document.getElementsByClassName('rows');
 const columns = document.getElementsByClassName('columns');
+const color = document.getElementById('color');
+const modes = document.querySelectorAll('.modes input');
 const grid = document.getElementsByClassName('grid');
 
-window.addEventListener('load', setNumberOfSquares);
-window.addEventListener('load', placeSquares);
+window.addEventListener('load', useDefaultValues);
 squares.addEventListener('input', setNumberOfSquares);
 squares.addEventListener('change', placeSquares);
-grid[0].addEventListener('mousemove', changeBackgroundColor);
+modes.forEach(mode => mode.addEventListener('change', useActivatedMode));
