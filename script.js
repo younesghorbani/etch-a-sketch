70ffeaf72d66dc92caa99c673e29441915a00c83
Younes Ghorbani <122295811+younesghorbani@users.noreply.github.com>
2023-03-13 19:58:26 +0330

    Fix missing HEX digit

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ function placeSquares() {
 }
 
 function generateRandomColor() {
-    const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 
     let randomColor = '#';
     for (let pairCounter = 0; pairCounter < 3; pairCounter++) {
         
         let pair = '';
         for (let digitCounter = 0; digitCounter < 2; digitCounter++) {
-            let position = Math.floor(Math.random() * 15);
+            let position = Math.floor(Math.random() * 16);
             pair += digits[position];
         }
 
